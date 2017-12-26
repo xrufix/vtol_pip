ce26521502a3a1c5b51effaab1f5cfa23e2806ef
rufix <stefan@ruhfass.com>
2017-12-26 20:06:49 +0100

    Add version to config

--- addons/xru_VTOL_PIP/config.cpp
+++ addons/xru_VTOL_PIP/config.cpp
@@ -1,10 +1,9 @@
 class CfgPatches {
     class xru_VTOL_PIP {
         name = "VTOL CCIP Mod";
+        version = 1.1.0.2;
         author = "xrufix";
-        url = "https://github.com/xrufix";
-        units[] = {};
-        weapons[] = {};
+        url = "https://github.com/xrufix/vtol_pip";
         requiredVersion = 1.8;
         requiredAddons[] = {"A3_Air_F_Exp_VTOL_01","A3_Weapons_F_Exp"};
     };
